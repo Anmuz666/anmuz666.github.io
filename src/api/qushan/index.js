813613b4d88d4e4e659ec502e94824c11d26a3ab
lkd <1469875883@qq.com>
2024-08-12 17:25:48 +0800

    dist

--- src/api/qushan/index.js
+++
@@ -1,2 +0,0 @@
-import axios from "axios";
-export default axios.get('./330921.geojson')
--- vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  publicPath: "/",
-  outputDir: "dist",
-  assetsDir:"static",
-
-  transpileDependencies: true
-})

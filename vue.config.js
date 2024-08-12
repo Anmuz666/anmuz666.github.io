1e83ff507164b15b44d17ab20148ed22ea9a0229
lkd <1469875883@qq.com>
2024-08-12 17:23:40 +0800

    11

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: "/",
+  outputDir: "dist",
+  assetsDir:"static",
+
   transpileDependencies: true
 })
